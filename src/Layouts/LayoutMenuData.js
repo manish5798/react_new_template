222863c22737601ff4f888e14d2b3a3d6298eaeb
manish5798 <manish@darsa.ai>
2022-12-19 16:31:25 +0530

    React Template

---
+++ src/Layouts/LayoutMenuData.js
@@ -0,0 +1,66 @@
+import React, { useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
+
+const Navdata = () => {
+  const history = useHistory();
+  //state data
+  const [isDashboard, setIsDashboard] = useState(false);
+  const [iscurrentState, setIscurrentState] = useState("Dashboard");
+
+  function updateIconSidebar(e) {
+    if (e && e.target && e.target.getAttribute("subitems")) {
+      const ul = document.getElementById("two-column-menu");
+      const iconItems = ul.querySelectorAll(".nav-icon.active");
+      let activeIconItems = [...iconItems];
+      activeIconItems.forEach((item) => {
+        item.classList.remove("active");
+        var id = item.getAttribute("subitems");
+        if (document.getElementById(id))
+          document.getElementById(id).classList.remove("show");
+      });
+    }
+  }
+
+  useEffect(() => {
+    document.body.classList.remove("twocolumn-panel");
+    if (iscurrentState !== "Dashboard") {
+      setIsDashboard(false);
+    }
+
+    if (iscurrentState === "Widgets") {
+      history.push("/widgets");
+      document.body.classList.add("twocolumn-panel");
+    }
+  }, [
+    history,
+    iscurrentState,
+    isDashboard,
+   
+  ]);
+
+  const menuItems = [
+    {
+      id: "dashboard",
+      label: "Dashboards",
+      icon: "ri-dashboard-2-line",
+      link: "/#",
+      stateVariables: isDashboard,
+      click: function (e) {
+        e.preventDefault();
+        setIsDashboard(!isDashboard);
+        setIscurrentState("Dashboard");
+        updateIconSidebar(e);
+      },
+      subItems: [
+        {
+          id: "crm",
+          label: "CRM",
+          link: "/dashboard",
+          parentId: "dashboard",
+        },
+      ],
+    },
+  ];
+  return <React.Fragment>{menuItems}</React.Fragment>;
+};
+export default Navdata;
